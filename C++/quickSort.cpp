080c6cd9912a2b6ea67f5fca901d71511ccde405
Rudra Tiwari <rudratiwari2901@gmail.com>
2021-11-12 12:32:13 +0530

    feat: Added Quicksort

---
+++ C++/quickSort.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <chrono>
+
+using namespace std;
+using namespace std::chrono;
+
+void swap(int &x, int &y) {
+    int temp = x;
+    x = y;
+    y = temp;
+}
+
+// Quick Sort Implementation
+int partition(int* arr,int l,int r){
+    int pivot = arr[r];
+    int i = l-1;
+
+    for(int j=l;j<r;j++){
+        if(arr[j]<pivot){
+            i++;
+            swap(arr[i],arr[j]);
+        }
+    }
+    swap(arr[i+1],arr[r]);
+    return i+1;
+}
+
+void quickSort(int *arr,int l,int r){
+    if(l<r){
+        int p = partition(arr,l,r);
+
+        quickSort(arr,l,p-1);
+        quickSort(arr,p+1,r);
+    }
+}
+
+void printArray(int arr[], int n) {
+    cout<<"\n Array : ";
+    for(int i = 0; i < n; i++)
+        cout<<arr[i]<<" ";
+}
+
+int main() {
+    //Input size
+    int n;
+    cin>>n;
+    
+    int arr[n];
+    for(int i = 0; i < n; i++)
+        cin>>arr[i];
+    
+    auto start = high_resolution_clock::now();
+    quickSort(arr, 0, n-1);
+    auto end = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(end-start);
+    
+    printArray(arr, n);
+    cout<<"\n Time Used : "<<duration.count()<<"microsecond \n";
+    
+    return 0;
+}
+
+
